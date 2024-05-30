cb71dd123c17f0c61eb0b883b81d14a5f303f690
Udit Singh <udit6706@gmail.com>
2024-05-30 15:44:36 +0530

    edit app,jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 function App() {
   return (
+    <>
     <div className='bg-slate-500 text-2xl'>
       kjsdfljsdhkjdshjl
     </div>
+    <h1>kjsdckbsdkjcbsdkjb</h1>
+    </>
   )
 }
 
